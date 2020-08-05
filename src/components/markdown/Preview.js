adac357b5bb17d5c22c90bf8b6cdd1b394e07ed2
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-05 14:29:53 -0700

    refactored Preview

--- src/components/markdown/Preview.js
+++ src/components/markdown/Preview.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import marked from 'marked';
 import styles from 'github-markdown-css/github-markdown.css';
+import { useMarkDownState } from '../../hooks/markDowncontext';
 
-const Preview = ({ markdown }) => {
-  const __html = marked(markdown);
+const Preview = () => {
+
+  const { state } = useMarkDownState();
+  const __html = marked(state.markdown);
   return <div className={styles['markdown-body']} dangerouslySetInnerHTML={{ __html }}></div>;
 };
 
-Preview.propTypes = {
-  markdown: PropTypes.string.isRequired
-};
+// Preview.propTypes = {
+//   markdown: PropTypes.string.isRequired
+// };
 
 export default Preview;
