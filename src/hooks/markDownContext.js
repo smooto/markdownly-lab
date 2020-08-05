2fcf928d41da8c9833c39a39c3bbdced44acb673
smooto <hello.smooto@gmail.com>
2020-08-05 15:56:00 -0700

    Incorporate feedback

--- src/hooks/markDownContext.js
+++ src/hooks/markDownContext.js
@@ -2,11 +2,6 @@ import React, { useContext } from 'react';
 
 export const MarkDownContext = React.createContext();
 
-export const useMarkDownState = () => {
-  const { state } = useContext(MarkDownContext); 
-  return state; 
-};
-
 export const useDispatch = () => {
   const { dispatch } = useContext(MarkDownContext); 
   return dispatch; 
--- src/selectors/markDownSelectors.js
+++ src/selectors/markDownSelectors.js
@@ -1,2 +1,7 @@
+import marked from 'marked';
+
 export const getMarkDowns = state => state.markdown; 
 
+export const getMarkDownsPreview = state => {
+  return marked(getMarkDowns(state));
+};
