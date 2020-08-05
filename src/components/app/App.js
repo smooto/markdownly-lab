8d35188123c218d14a3236677fd55c8016b2438c
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-05 15:23:14 -0700

    adding what I have so far for landing page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Document from '../../containers/Document';
+import { Router } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <h1>Markdown Editor</h1>
       <Document />
-    </>
+    </Router>
+  
   );
 }
---
+++ src/containers/landingpage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const LandingPage = () => {
+  return (
+    <div>
+      {/* SearchBar, List of Markdowns, Filter */}
+    </div>
+  );
+};
+
+
+export default LandingPage;
+
