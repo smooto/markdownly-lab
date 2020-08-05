6efab3c6c0f7484ca5d033b58b0d0db439259e08
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-05 14:28:09 -0700

    added refactoring to Editor

--- src/components/markdown/Editor.js
+++ src/components/markdown/Editor.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Editor.css';
+import { useMarkDownState, useDispatch } from '../../hooks/markDowncontext';
+// import { updateMarkDown } from '../../actions/markDownActions';
 
-const Editor = ({ markdown, updateMarkdown }) => (
-  <textarea className={styles.Editor} value={markdown} onChange={updateMarkdown} />
-);
 
-Editor.propTypes = {
-  markdown: PropTypes.string.isRequired,
-  updateMarkdown: PropTypes.func.isRequired
+const Editor = () => {
+  const { state } = useMarkDownState();
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target }) => {
+    dispatch({ 
+      type: 'UPDATE_MARKDOWN', 
+      payload: target.value
+    });
+  };
+
+  <textarea className={styles.Editor} value={state.markdown} onChange={handleChange} />;
 };
+
+// Editor.propTypes = {
+//   markdown: PropTypes.string.isRequired,
+//   // updateMarkdown: PropTypes.func.isRequired
+// };
 
 export default Editor;
