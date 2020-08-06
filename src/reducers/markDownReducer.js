85ec159fed95f3bef7a95b7ffb59cc80c01e9922
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-06 13:46:03 -0700

    updates state management for reducer

--- src/reducers/markDownReducer.js
+++ src/reducers/markDownReducer.js
@@ -1,25 +1,30 @@
 export const initialState = {
-  currentMarkDownTitle: 'title',
+  currentMarkDownId: '001',
   markdownArray: [{
-
-    title: 'content',
-    'another title': 'content'
-    // body: ''
-    // id: Date.now()
+    id: '001',
+    title: '',
+    body: ''
   }],
 };
-
-// to update the current markdown, spread state, state.markdownArray.map(markdown(this is a markdown object within array) => if markdown.id === state.currentId return ...markdown, body: action.payload else return markdown)
-// markdown will always create a new array, so when we map above, it will create new array 
 
 export default function reducer(state, action) {
   switch(action.type){
     case 'UPDATE_MARKDOWN':
-      return { ...state, markdown: { ...state.markdown, } };
+      return { ...state, markdownArray: state.markdownArray.map(markdown => {
+        if(markdown.id === state.currentMarkDownId) {
+          return markdown.body = action.payload;
+        }
+        else return markdown;
+      }) };
+
     case 'CREATE_MARKDOWN':
-      return { ...state, markdown: { ...state.markdown, [action.payload]: '' } };
-    case 'SET_CURRENT_MARKDOWN_TITLE': 
-      return { ...state, currentMarkDownTitle: action.payload };
+      return { ...state,
+        markdownArray:  state.markdownArray.concat({
+          id: Date.now(), title: action.payload, body: '' }) };
+
+    case 'SET_CURRENT_MARKDOWN_ID':
+      return { ...state, currentMarkDownId: action.payload };
+
     default:
       return state;
   }
--- src/selectors/markDownSelectors.js
+++ src/selectors/markDownSelectors.js
@@ -1,18 +1,23 @@
 import marked from 'marked';
 
 // get content for file "in focus"
-export const getCurrentMarkDown = state => state.markdown[state.currentMarkDownTitle];
+export const getCurrentMarkDown = state => state.markdownArray.filter(markdown => {
+  if(markdown.id === state.currentMarkDownId) {
+    return markdown;
+  }
+});
 
 // basic get -- returns whole object
-export const getMarkDowns = state => state.markdown;
+export const getMarkDowns = state => state.markdownArray;
 
 // gets titles of all documents, returns array
 export const getMarkDownTitles = state => {
-  return Object.keys(getMarkDowns(state));
+  return state.markdownArray.map(markdown =>  markdown.title);
 };
 
 export const getMarkDownsPreview = state => {
-  return marked(getCurrentMarkDown(state));
+  const currentBody = getCurrentMarkDown(state).body;
+  return marked(currentBody);
 };
 
 // landing page: array of all titles
